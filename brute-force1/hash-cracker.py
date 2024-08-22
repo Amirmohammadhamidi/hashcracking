976a3eab01f39c2436b3032b2dd49a732f9715c5
amirmohammadhamidi <amirmohammadhmd.20@gmail.com>
2024-08-23 00:28:22 +0330

    multithreading completed format for six charcter passwords

--- brute-force1/hash-cracker.py
+++ brute-force1/hash-cracker.py
@@ -11,15 +11,17 @@ charset = list(
 def check_password_validity(hash: str, password: str, type: str):
     if (type == "md5"):
         return (hashlib.md5(password.encode()).hexdigest() == hash)
+    elif (type == "sha1"):
+        return (hashlib.sha1(password.encode()).hexdigest() == hash)
+    elif (type == "sha224"):
+        return (hashlib.sha224(password.encode()).hexdigest() == hash)
+    elif (type == "sha256"):
+        return (hashlib.sha256(password.encode()).hexdigest() == hash)
+    elif (type == "sha384"):
+        return (hashlib.sha384(password.encode()).hexdigest() == hash)
 
-    # def crack_two_digit_hash(hash: str, type: str, combination: str):
 
-
-# def crack_six_loop(hash: str, type: str, word: str):
-
-
-def crack_fifth_loop(hash: str, type: str, word: str):
-    # 3 min
+def crack_six_loop(hash: str, type: str, word: str):
     global password_cracked
     if (password_len == 5):
         for counter in range(70):
@@ -29,9 +31,24 @@ def crack_fifth_loop(hash: str, type: str, word: str):
                 password_cracked = True
                 print("password: ", word + charset[counter])
                 break
-    # else:
+
+
+def crack_fifth_loop(hash: str, type: str, word: str):
+    # ~3 mins
+    global password_cracked
+    # if (password_len == 5):
     #     for counter in range(70):
-    #         crack_six_loop(hash, type, word + charset[counter])
+    #         if (password_cracked):
+    #             break
+    #         if (check_password_validity(hash, word + charset[counter], type)):
+    #             password_cracked = True
+    #             print("password: ", word + charset[counter])
+    #             break
+    # else:
+    for counter in range(70):
+        crack_six_loop(hash, type, word + charset[counter])
+        if (password_cracked):
+            break
 
 
 def crack_fourth_loop(hash: str, type: str, word: str):
@@ -84,7 +101,6 @@ def crack_second_loop(hash: str, type: str, word: str):
         thread = threading.Thread(
             target=crack_third_loop, args=(hash, type, word + charset[counter]))
         thread.start()
-        # print("thread with index : ", counter, " created")
         if (password_cracked):
             break
 
@@ -102,21 +118,18 @@ def crack_first_loop(hash: str, type: str):
         thread = threading.Thread(
             target=crack_second_loop, args=(hash, type, charset[counter]))
         thread.start()
-        # print("thread with index : ", counter, " created")
         if (password_cracked):
             break
 
 
-def handle_md5():
-    target_hash = input("enter target hash : ")
-    print("wait for cracking...")
-    crack_first_loop(target_hash, "md5")
+def crack_password(target_hash: str, type="md5"):
+    crack_first_loop(target_hash, type)
 
 
-def handle_thread_number():
+def calculate_password_len():
     global password_len
     while (True):
-        password_len_str = input("password characters number :")
+        password_len_str = input("password characters number(default == 5) :")
         try:
             password_len = int(password_len_str)
             if (password_len < 1 or password_len > 8):
@@ -128,22 +141,37 @@ def handle_thread_number():
             password_len = 7
             print("input must be int")
 
-    # we will make it more accurate
-
 
 print("options : ")
 print("[1]:md5_hashCracker")
-print("[2]:default_hashCracker")
-print("[3]:exit")
+print("[2]sha1 hashCracker")
+print("[3]sha224 hashCracker")
+print("[4]sha256 hashCracker")
+print("[5]sha384 hashCracker")
+print("[6]:default_hashCracker")
+print("[7]:exit")
 selected_option = input("enter target mode : ")
 while (True):
     try:
         selected_option_index = int(selected_option)
-        handle_thread_number()
-        if (selected_option_index == 1):
-            handle_md5()
-        elif (selected_option_index == 3):
+        if (selected_option_index == 7):
             print("bye!")
             break
+        calculate_password_len()
+        target_hash = input("enter target hash : ")
+        print("wait for cracking...")
+        if (selected_option_index == 1):
+            crack_password(target_hash)
+        elif (selected_option_index == 2):
+            crack_password(target_hash, "sha1")
+        elif (selected_option_index == 3):
+            crack_password(target_hash, "sha224")
+        elif (selected_option_index == 4):
+            crack_password(target_hash, "sha256")
+        elif (selected_option_index == 5):
+            crack_password(target_hash, "sha384")
+        else:
+            print("invalid index")
+
     except ValueError:
         print("invalid input format")
